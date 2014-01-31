0125bc0b354d38c4cb5e5065a9a0db2aca84f129
Emil Tin <emil.tin@tmf.kk.dk>
2014-01-31 10:52:40 +0100

    more status output work on mac too

--- features/support/launch.rb
+++ features/support/launch.rb
@@ -42,7 +42,7 @@ class OSRMLoader
 
   def self.osrm_up?
     if @@pid
-      s = `ps -o state -p #{@@pid} --no-headers`.strip
+      s = `ps -o state -p #{@@pid}`.split[1].to_s.strip
       up = (s =~ /^[DRST]/) != nil
       puts "=== osrm-routed, status pid #{@@pid}: #{s} (#{up ? 'up' : 'down'})"
       up
